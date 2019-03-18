b5fe13573bff541776570bd77e713bfb0cf2210a
Gareth Funk <gareth.funk94@gmail.com>
2019-03-18 23:25:55 +0000

    Added support for streaks. Tested (briefly) on 2018 data. Looks to work out correctly as far as I can tell.

--- api.py
+++ api.py
@@ -10,7 +10,7 @@ import json
 
 import scoring
 
-season = "2019"
+season = "2018"
 api_base_url = "https://ergast.com/api/f1/" + season + "/"
 
 def getConstructors():
@@ -19,6 +19,8 @@ def getConstructors():
     for constructor in json.loads(req.content)["MRData"]["ConstructorTable"]["Constructors"]:
         constructors[constructor["constructorId"]] = constructor
         constructors[constructor["constructorId"]]["score"] = 0
+        constructors[constructor["constructorId"]]["qualy_streak"] = 0
+        constructors[constructor["constructorId"]]["race_streak"] = 0
     return constructors
 
 def getDrivers():
@@ -27,29 +29,35 @@ def getDrivers():
     for driver in json.loads(req.content)["MRData"]["DriverTable"]["Drivers"]:
         drivers[driver["code"]] = driver
         drivers[driver["code"]]["score"] = 0
+        drivers[driver["code"]]["qualy_streak"] = 0
+        drivers[driver["code"]]["race_streak"] = 0
     return drivers
 
 def qualy(drivers, constructors):
-    req = requests.get(api_base_url + "qualifying.json")
-    for race in json.loads(req.content)["MRData"]["RaceTable"]["Races"]:
+    req = requests.get(api_base_url + "qualifying.json?limit=9999")
+    all_qualy_data = json.loads(req.content)["MRData"]["RaceTable"]["Races"]
+    # Score qualy individually
+    for race in all_qualy_data:
         for result in race["QualifyingResults"]:
             scoring.scoreQualy(result, race["QualifyingResults"], drivers, constructors)
 
 def race(drivers, constructors):
-    req = requests.get(api_base_url + "results.json")
-    for race in json.loads(req.content)["MRData"]["RaceTable"]["Races"]:
+    req = requests.get(api_base_url + "results.json?limit=9999")
+    all_race_data = json.loads(req.content)["MRData"]["RaceTable"]["Races"]
+    # Score qualy individually 
+    for race in all_race_data:
         for result in race["Results"]:
             scoring.scoreRace(result, race["Results"], drivers, constructors)
             
-            
-
 if __name__ == "__main__":
     constructors = getConstructors()
     drivers = getDrivers()
     qualy(drivers, constructors)
     race(drivers, constructors)
-    for driver in drivers.values():
-        print(driver["code"] + " = " + str(driver["score"]))
-    for constructor in constructors.values():
-        print(constructor["constructorId"] + " = " + str(constructor["score"]))
+    driver_scores = sorted([(x["code"], x["score"]) for x in drivers.values()], key = lambda x:x[1], reverse=True)
+    constructor_scores = sorted([(x["constructorId"], x["score"]) for x in constructors.values()], key = lambda x:x[1], reverse=True)
+    for driver in driver_scores:
+        print(driver[0] + " = " + str(driver[1]))
+    for constructor in constructor_scores:
+        print(constructor[0] + " = " + str(constructor[1]))
     pass 
--- scoring.py
+++ scoring.py
@@ -4,6 +4,10 @@
 Gareth Funk 2019
 API Call to the ergast API.
 Scoring based on https://fantasy.formula1.com/points-scoring
+On Streaks: When a driver or constructor achieves a streak, that streak will
+reset and must be built up again. For example: A driver achieves three top
+tens in a row. He will be awarded a streak, but must achieve another three top
+tens in a row to get streak points for a second time.
 """
 
 race_position_bonus_lookup = [25, 18, 15, 12, 10, 8, 6, 4, 2, 1]
@@ -35,6 +39,23 @@ def scoreQualy(qualy_result, full_qualy_results, drivers, constructors):
     qualy_position_bonus = max(11 - qualy_pos, 0)
     drivers[driver_code]["score"] += qualy_position_bonus
     constructors[constructorId]["score"] += qualy_position_bonus
+    # Streak Counter
+    if qualy_pos <= 10:
+        drivers[driver_code]["qualy_streak"] += 1
+        if teamMateInTopTen(constructorId, qualy_pos, full_qualy_results):
+            constructors[constructorId]["qualy_streak"] += 0.5  # When we see the team mate, we will increment this again
+        # Driver Qualifying - driver qualifies in Top 10 for 5 races in a row = 5 pts
+        if drivers[driver_code]["qualy_streak"] == 5:
+            drivers[driver_code]["qualy_streak"] = 0  # Reset
+            drivers[driver_code]["score"] += 5
+        # Constructor Qualifying - both drivers qualify in Top 10 for 3 races in a row = 5 pts
+        if constructors[constructorId]["qualy_streak"]  == 3:
+            constructors[constructorId]["qualy_streak"] = 0  # Reset
+            constructors[constructorId]["score"] += 5
+    else:
+        drivers[driver_code]["qualy_streak"] = 0  # Reset
+        constructors[constructorId]["qualy_streak"] = 0  # Reset
+    return
 
 def scoreRace(result, full_results, drivers, constructors):
     driver_code = result["Driver"]["code"]
@@ -80,8 +101,23 @@ def scoreRace(result, full_results, drivers, constructors):
         else:
             # Not classifed = -15 pts (driver only)
             drivers[driver_code]["score"] += -15
-    
-
+    # Streak Counter
+    if final_pos <= 10:
+        drivers[driver_code]["race_streak"] += 1
+        if teamMateInTopTen(constructorId, final_pos, full_results):
+            constructors[constructorId]["race_streak"] += 0.5  # When we see the team mate, we will increment this again
+        # Driver Race - driver finishes race in Top 10 for 5 races in a row = 10 pts
+        if drivers[driver_code]["race_streak"] == 5:
+            drivers[driver_code]["race_streak"] = 0  # Reset
+            drivers[driver_code]["score"] += 10
+        # Constructor Race - both drivers finish race in Top 10 for 3 races in a row = 10 pts
+        if constructors[constructorId]["race_streak"]  == 3:
+            constructors[constructorId]["race_streak"] = 0  # Reset
+            constructors[constructorId]["score"] += 10
+    else:
+        drivers[driver_code]["race_streak"] = 0  # Reset
+        constructors[constructorId]["race_streak"] = 0  # Reset
+    return
 
 def racePositionBonus(race_position):
     if race_position <= 10:
@@ -110,5 +146,27 @@ def beatTeamMate(constructorId, driverPos, full_results):
                     # Team mate had non-numeric, driver beat team mate by default.
                     return True
                 if teammatePos > driverPos:
+                    return True
+    return False
+
+def teamMateInTopTen(constructorId, driverPos, full_results):
+    try:
+        driverPos = int(driverPos)
+    except ValueError:
+        raise ValueError("This function shouldn't be called for drivers not in the top 10 already")
+    for result in full_results:
+        if result["Constructor"]["constructorId"] == constructorId:
+            # Same team
+            if int(result["position"]) == driverPos:
+                # Same person
+                continue
+            else:
+                # This is the team mate
+                try:
+                    teammatePos = int(result["position"])
+                except ValueError:
+                    # Team mate had non-numeric, not in top 10
+                    return False
+                if teammatePos <= 10:
                     return True
     return False
