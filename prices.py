a9de9e7f4b734a609d797cdcfd8029b2e857737e
Gareth Funk <gareth.funk94@gmail.com>
2019-03-18 23:59:27 +0000

    Added some pricing data.

---
+++ prices.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+"""
+Gareth Funk 2019
+"""
+
+# Prices indexed by race. Index 0 = before 1st race. Index 1 = after first race, before second race.
+constructor_prices = [{ 'mercedes':32.0,  # Pre-1st race data same as pre-2nd race data. Slightly stale!
+                        'ferrari':29.0,
+                        'red_bull':25.0,
+                        'haas':9.5,
+                        'renault':12.5,
+                        'toro_rosso':6.2,
+                        'williams':6.0,
+                        'alfa':9.5,
+                        'racing_point':9.7,
+                        'mclaren':7.4},
+                        { 'mercedes':32.0,  # Pre-2nd race data collected 18/3/2019
+                        'ferrari':29.0,
+                        'red_bull':25.0,
+                        'haas':9.5,
+                        'renault':12.5,
+                        'toro_rosso':6.2,
+                        'williams':6.0,
+                        'alfa':9.5,
+                        'racing_point':9.7,
+                        'mclaren':7.4},
+                     ]
+
+driver_prices = [{  'BOT':20.5, # Pre-1st race data same as pre-2nd race data. Slightly stale!
+                    'VER':24.5,
+                    'HAM':30.5,
+                    'VET':27.5,
+                    'HUL':11.8,
+                    'MAG':10.0,
+                    'LEC':23.5,
+                    'KVY':5.5,
+                    'RAI':10.0,
+                    'STR':7.7,
+                    'RUS':6.0,
+                    'GAS':17.5,
+                    'KUB':5.5,
+                    'ALB':6.0,
+                    'NOR':6.9,
+                    'GIO':6.8,
+                    'PER':11.0,
+                    'GRO':9.3,
+                    'RIC':12.0,
+                    'SAI':7.7},
+                 {  'BOT':20.5,  # Pre-2nd race data collected 18/3/2019
+                    'VER':24.5,
+                    'HAM':30.5,
+                    'VET':27.5,
+                    'HUL':11.8,
+                    'MAG':10.0,
+                    'LEC':23.5,
+                    'KVY':5.5,
+                    'RAI':10.0,
+                    'STR':7.7,
+                    'RUS':6.0,
+                    'GAS':17.5,
+                    'KUB':5.5,
+                    'ALB':6.0,
+                    'NOR':6.9,
+                    'GIO':6.8,
+                    'PER':11.0,
+                    'GRO':9.3,
+                    'RIC':12.0,
+                    'SAI':7.7},
+                ]
